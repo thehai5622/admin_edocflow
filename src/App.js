509070105e8b21bebe0175db391ec255d6c93f0d
HaiLT <luonghai5622@gmail.com>
2025-06-30 22:29:42 +0700

    tmep

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Users from "./scenes/user";
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 import Login from "./scenes/login";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateUser from "./scenes/user/CreateUser";
@@ -77,17 +67,6 @@ const MainLayout = () => {
 
             {/* Cấp hành chính */}
             <Route path="/administrativelevel" element={<AdministrativeLevel />} />
-
-            <Route path="/team" element={<Team />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/invoices" element={<Invoices />} />
-            <Route path="/form" element={<Form />} />
-            <Route path="/bar" element={<Bar />} />
-            <Route path="/pie" element={<Pie />} />
-            <Route path="/line" element={<Line />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/geography" element={<Geography />} />
           </Routes>
         </main>
       </div>
